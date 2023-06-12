23c4b16e0411c067bf1eeeb4b5a2d74fcc0a67d2
caua075 <caua.andradeg07@gmail.com>
2023-06-12 08:06:48 -0300

    commit caua

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from Ingrediente import Ingrediente
 from Receita import Receita
+from IngredienteReceita import RelacaoReceitaIngrediente
 
 # Lista de ingredientes
 ingredientes = []
@@ -81,8 +82,75 @@ def atualizar_receita(receitas):
             return
     print("Receita não encontrada.")
 
-# Lista de relações entre receitas e ingredientes
+# Lista de relação Receita X Ingredientes
 relacoes = []
+
+#Função para criar uma relação Receita X Ingredientes
+def adicionar_ingredientes(receita):
+    ingrediente_id = int(input("Digite o ID do ingrediente: "))
+    ingrediente = None
+    for i in ingredientes:
+        if i.id_ing == int(ingrediente_id):
+            ingrediente = i
+            break
+    if ingrediente is None:
+        print(f"Ingrediente com ID {ingrediente_id} não encontrado.")
+        return
+    ingredientes.append(ingrediente)
+    qtd = input("Informe a quantidade desse ingrediente:")
+    relacao = RelacaoReceitaIngrediente(receita, ingredientes, qtd)
+    return relacao
+
+#Função para listar a relação Receita X Ingredientes
+def listar_ingredientes_da_receita(receita, relacoes, ingredientes):
+    ingredien = []
+    for relacao in relacoes:
+        if relacao.receita == receita:
+            ingredien.append(relacao)
+
+    if len(ingredien) == 0:
+        print("Não há ingredientes cadastrados para esta receita.")
+    else:
+        print(f"Ingredientes da receita '{receita.nome_rec}':")
+        for ingre in ingredien:
+            pesquisa_ingrediente(ingre.ingrediente, ingredientes)
+
+#Função para excluir uma relação Receita X Ingredientes
+def remover_relacao_receita_ingredientes(receita, relacoes):
+    for relacao in relacoes:
+        if relacao.receita == receita:
+            relacoes.remove(relacao)
+            print("Relação entre receita e ingredientes removida com sucesso.")
+            return
+    print("Relação entre receita e ingredientes não encontrada.")
+
+#Função para alterar um relação Receita X Ingredientes
+def atualizar_relacao_receita_ingredientes(receita, relacoes):
+    for relacao in relacoes:
+        if relacao.receita == receita:
+            novo_quantidade = float(input(f"Digite a nova quantidade de {relacao.ingrediente.nome_ing} necessária: "))
+            relacao.quantidade = novo_quantidade
+            print("Relação entre receita e ingredientes atualizada com sucesso.")
+            return
+    print("Relação entre receita e ingredientes não encontrada.")
+
+def pesquisa_ingrediente(id_ing, ingredientes):
+    qtd = 0
+    for ingrediente in ingredientes:
+        if id_ing == ingrediente.id_ing:
+            qtd=1
+            print(f"ID: {ingrediente.id_ing} | Nome: {ingrediente.nome_ing}")
+    if qtd==0:
+        print("Sem ingrediente!")
+
+def pesquisa_receita(id_ing, ingredientes):
+    qtd = 0
+    for ingrediente in ingredientes:
+        if id_ing == ingrediente.id_rec:
+            qtd=1
+            print(f"ID: {ingrediente.id_rec} | Nome: {ingrediente.nome_rec} | Autor: {ingrediente.autor_rec}")
+    if qtd==0:
+        print("Sem ingrediente!")
 
 # Loop para o usuário interagir com o programa
 while True:
@@ -98,6 +166,9 @@ while True:
     print("9. Adicionar Ingredientes à Receita")
     print("10. Listar Ingredientes de uma Receita")
     print("11. Remover Relação entre Receita e Ingredientes")
+    print("12. Atualizar Ingredientes de uma Receita")
+    print("13. Pesquisar Ingrediente")
+    print("14. Pesquisar Receita")
     print("0. Sair")
 
     opcao = input("Escolha uma opção: ")
@@ -123,11 +194,40 @@ while True:
     elif opcao == "8":
         atualizar_receita(receitas)
     elif opcao == "9":
-        pass
+        id_rec = int(input("Digite o ID da receita: "))
+        receita = None
+        for r in receitas:
+            if r.id_rec == id_rec:
+                receita = r
+                break
+        if receita is None:
+            print("Receita não encontrada.")
+        else:
+            relacao = adicionar_ingredientes(receita)
+            if relacao:
+                relacoes.append(relacao)
+                print("Ingredientes adicionados à receita com sucesso!")
     elif opcao == "10":
-        pass
+        id_rec = int(input("Digite o ID da receita: "))
+        receita = None
+        for r in receitas:
+            if r.id_rec == id_rec:
+                receita = r
+                break
+        if receita is None:
+            print("Receita não encontrada.")
+        else:
+            listar_ingredientes_da_receita(receita, relacoes, ingredientes)
     elif opcao == "11":
-        pass
+        remover_relacao_receita_ingredientes(receita, relacoes)
+    elif opcao == "12":
+        atualizar_relacao_receita_ingredientes(receita, relacoes)
+    elif opcao== "13":
+        id = int(input("Informe o id do ingrediente:"))
+        pesquisa_ingrediente(id, ingredientes)
+    elif opcao== "14":
+        id = int(input("Informe o id da receita:"))
+        pesquisa_ingrediente(id, receitas)
     elif opcao == "0":
         break
     else:
